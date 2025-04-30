e15ddcebe335a2c1c9ac4239167a4ea23340aec2
LeoGura1987 <109128943+LeoGura1987@users.noreply.github.com>
2025-04-30 10:00:59 +0800

    ✅ 將編譯後的 Web 靜態頁面也加入 main 分支

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,11 +22,17 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1234eb3d089a59a9625fb313ff14bc81",
-"index.html": "9207ce76a952a73aa82b02e9a7dfbe4c",
-"/": "9207ce76a952a73aa82b02e9a7dfbe4c",
+"flutter_bootstrap.js": "c8dfdef705aab58ce15a1e1ea19dec4f",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "60d089e775623e5ecb51a0775eccf6d5",
+"/": "60d089e775623e5ecb51a0775eccf6d5",
 "main.dart.js": "4b600404984c45e9ae71b0017ece8ffb",
+"manifest.json": "bd9faa33c571feb28f53ab75257b4630",
 "version.json": "ae40d8107546d4e10fff129b682eb3c1"};
 // The application shell files that are downloaded before a service worker can
 // start.
